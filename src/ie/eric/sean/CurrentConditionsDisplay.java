d9fe07b944403f3f958ee35c9ea9a28c68a2b11e
Sean McGrath <smcgrath@SIDERO.Athlone.Local>
2017-09-13 13:03:02 +0100

    Added a new display class

---
+++ src/ie/eric/sean/CurrentConditionsDisplay.java
@@ -0,0 +1,27 @@
+package ie.eric.sean;
+
+/**
+ * Created by smcgrath on 13/09/2017.
+ */
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData){
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float termp, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degree and " + humidity + "% humidity");
+    }
+}
